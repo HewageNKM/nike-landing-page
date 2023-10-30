169411d9aa10c33febcafd59b842ea3a63f64a8b
hnkma <hnkmalwenna@gmail.om>
2023-10-30 21:04:17 +0530

    Nav Complete

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import Services from './Services';
+import PopularProducts  from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe"
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers   from "./SpecialOffers";
+
+export {
+    Hero,
+    Footer,
+    Services,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffers
+}
